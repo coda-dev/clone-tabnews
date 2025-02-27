770634dfceda9739cf9d4c794a9f1e2bfc3dbdaf
Edwin <62716970+coda-dev@users.noreply.github.com>
2025-02-27 01:59:29 +0000

    fix `/migrations` endpoint bug conexao

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,38 +1,51 @@
 import rodarMigrations from "node-pg-migrate";
 import { join } from "node:path"; // trata o caminho do arquivo sitema operacional
 import database from "infra/database";
+import { throws } from "node:assert";
 
 export default async function migrataions(request, response) {
-  const dbClient = await database.getNewClient();
-
-  const opcaoPadraoMigrataions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigraations",
-  };
-
-  if (request.method === "GET") {
-    const migrataionsPendentes = await rodarMigrations(opcaoPadraoMigrataions);
-    await dbClient.end(); // tem que encerrar conexao depois rodar porque senao gera lock
-    return response.status(200).json(migrataionsPendentes);
+  const allowedMethod = ["GET", "POST"];
+  if (!allowedMethod.includes(request.method)) {
+    return response.status(405).end({
+      error: `Method "${request.method}" not allowed`,
+    }); // 405 - significa não acho o methodos acima
   }
 
-  if (request.method === "POST") {
-    const migracoesMigrataions = await rodarMigrations({
-      ...opcaoPadraoMigrataions,
-      dryRun: false,
-    });
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
 
-    await dbClient.end(); // tem que encerrar conexao depois rodar porque senao gera lock
+    const opcaoPadraoMigrataions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigraations",
+    };
 
-    if (migracoesMigrataions.length > 0) {
-      return response.status(201).json(migracoesMigrataions); // 201 - altecao foi criada a migration
+    if (request.method === "GET") {
+      const migrataionsPendentes = await rodarMigrations(
+        opcaoPadraoMigrataions,
+      );
+      return response.status(200).json(migrataionsPendentes);
     }
-    return response.status(200).json(migracoesMigrataions);
-  }
 
-  return response.status(405).end(); // 405 - significa não acho o methodos acima
+    if (request.method === "POST") {
+      const migracoesMigrataions = await rodarMigrations({
+        ...opcaoPadraoMigrataions,
+        dryRun: false,
+      });
+
+      if (migracoesMigrataions.length > 0) {
+        return response.status(201).json(migracoesMigrataions); // 201 - altecao foi criada a migration
+      }
+      return response.status(200).json(migracoesMigrataions);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end(); // tem que encerrar conexao depois rodar porque senao gera lock
+  }
 }
