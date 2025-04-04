b5943591d1686cf12fa0db546c3ddc8fa789064d
Edwin <62716970+coda-dev@users.noreply.github.com>
2025-04-04 13:29:20 +0000

    refactor: `api/v1/migrations` rafatorada para receber model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,7 @@
-import rodarMigrations from "node-pg-migrate";
-import { resolve } from "node:path"; // trata o caminho do arquivo sitema operacional
-import database from "infra/database";
-//import { throws } from "node:assert";
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
+import migration from "models/migrator.js";
+
 const router = createRouter();
 
 router.get(getHandler);
@@ -11,45 +9,16 @@ router.post(postHandler);
 
 export default router.handler(controller.errorHandlers);
 
-const opcaoPadraoMigrataions = {
-  dryRun: true,
-  dir: resolve("infra", "migrations"),
-  direction: "up",
-  verbose: true,
-  migrationsTable: "pgmigraations",
-};
-
 async function getHandler(request, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-
-    const migrataionsPendentes = await rodarMigrations({
-      ...opcaoPadraoMigrataions,
-      dbClient,
-    });
-    return response.status(200).json(migrataionsPendentes);
-  } finally {
-    await dbClient?.end(); // tem que encerrar conexao depois rodar porque senao gera lock
-  }
+  const pendingMigrataions = await migration.listPendingMigrations();
+  return response.status(200).json(pendingMigrataions);
 }
 
 async function postHandler(request, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
+  const migracoesMigrataions = await migration.runPendingMigrations();
 
-    const migracoesMigrataions = await rodarMigrations({
-      ...opcaoPadraoMigrataions,
-      dbClient,
-      dryRun: false,
-    });
-
-    if (migracoesMigrataions.length > 0) {
-      return response.status(201).json(migracoesMigrataions); // 201 - altecao foi criada a migration
-    }
-    return response.status(200).json(migracoesMigrataions);
-  } finally {
-    await dbClient?.end(); // tem que encerrar conexao depois rodar porque senao gera lock
+  if (migracoesMigrataions.length > 0) {
+    return response.status(201).json(migracoesMigrataions); // 201 - altecao foi criada a migration
   }
+  return response.status(200).json(migracoesMigrataions);
 }
