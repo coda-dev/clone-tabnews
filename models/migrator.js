5c608ab6a90a7cb22583f0d86e26b96f852595e7
Edwin <62716970+coda-dev@users.noreply.github.com>
2025-04-04 13:27:46 +0000

    feat: criando o models para migrations

---
+++ models/migrator.js
@@ -0,0 +1,51 @@
+import rodarMigrations from "node-pg-migrate";
+import { resolve } from "node:path"; // trata o caminho do arquivo sitema operacional
+import database from "infra/database.js";
+//import { throws } from "node:assert";
+
+const opcaoPadraoMigrataions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigraations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migrataionsPendentes = await rodarMigrations({
+      ...opcaoPadraoMigrataions,
+      dbClient,
+    });
+    return migrataionsPendentes;
+  } finally {
+    await dbClient?.end(); // tem que encerrar conexao depois rodar porque senao gera lock
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+
+    const migracoesMigradas = await rodarMigrations({
+      ...opcaoPadraoMigrataions,
+      dbClient,
+      dryRun: false,
+    });
+
+    return migracoesMigradas;
+  } finally {
+    await dbClient?.end(); // tem que encerrar conexao depois rodar porque senao gera lock
+  }
+}
+
+const migration = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migration;
