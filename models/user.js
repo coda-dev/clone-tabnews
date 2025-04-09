ee2919bbf8aa796865aa29e775eac5788cac71a8
Edwin <62716970+coda-dev@users.noreply.github.com>
2025-04-09 19:24:59 +0000

    build: add verificacao para um usuario com `username`

--- models/user.js
+++ models/user.js
@@ -1,6 +1,35 @@
 import dataBase from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
 
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const results = await dataBase.query({
+      text: `
+      SELECT 
+        * 
+      FROM 
+        users
+      WHERE
+        LOWER(username) = LOWER($1)
+      LIMIT
+        1
+      ;`,
+      values: [username],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
   await validateUniqueUsername(userInputValues.username);
@@ -73,5 +102,6 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findOneByUsername,
 };
 export default user;
